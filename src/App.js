78004234556e17d123cc36377d3d14c39d153ea7
vishwanath <vishwanath.t@codilar.com>
2021-12-28 15:59:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Short_link from './components/short_link';
+
+function App() {
+  return (
+    <div className="App">
+      <Short_link/>
+    </div>
+  );
+}
+
+export default App;
